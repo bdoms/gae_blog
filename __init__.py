141f7b53f63706f5ede05ea843a7371ebda6b7bc
bdoms <ravenize@gmail.com>
2011-06-22 20:28:44 -0500

    Fixed continuing import issues.

--- __init__.py
+++
@@ -1,6 +0,0 @@
-
-import os
-import sys
-PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(PARENT_DIR)
-
--- blog.py
+++ blog.py
@@ -1,5 +1,11 @@
 # this is the main entry point for the application
 
+import os
+import sys
+PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if PARENT_DIR not in sys.path:
+    sys.path.append(PARENT_DIR)
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
