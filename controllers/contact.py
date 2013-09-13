bdoms/gae_blog

commit e364fe4dd843a77a68dc2f1bb1b1779526398a20

Fixed a bug with spam bots sending non-unicode characters in forms.

controllers/contact.py
@@ -26,10 +26,13 @@ class ContactController(BaseController):
 
         blog = self.getBlog()
         if blog and blog.contact:
-            author_slug = self.request.get("author")
-            email = self.request.get("email")
-            subject = self.request.get("subject", "")
-            body = self.request.get("body")
+            try:
+                author_slug = self.request.get("author")
+                email = self.request.get("email")
+                subject = self.request.get("subject", "")
+                body = self.request.get("body")
+            except UnicodeDecodeError:
+                return seld.renderError(400)
 
             errors = {}
             form_data = {"author": author_slug, "email": email, "subject": subject, "body": body}

controllers/post.py
@@ -32,12 +32,15 @@ class PostController(BaseController):
                 if post and post.published:
                     # only allow commenting to a post if it's actually published
 
-                    author_choice = self.request.get("author-choice")
-                    author_slug = self.request.get("author")
-                    name = self.request.get("name")
-                    url = self.request.get("url")
-                    email = self.request.get("email")
-                    body = self.request.get("body", "")
+                    try:
+                        author_choice = self.request.get("author-choice")
+                        author_slug = self.request.get("author")
+                        name = self.request.get("name")
+                        url = self.request.get("url")
+                        email = self.request.get("email")
+                        body = self.request.get("body", "")
+                    except UnicodeDecodeError:
+                        return seld.renderError(400)
 
                     errors = {}
                     form_data = {"author-choice": author_choice, "author": author_slug, "name": name, "url": url, "email": email, "body": body}

commit 1353e4aefed12d362ed5d72cae363b4a91ddb16d

Fixed a typo introduced in e364fe4dd843a77a68dc2f1bb1b1779526398a20

controllers/contact.py
@@ -33,7 +33,7 @@ class ContactController(BaseController):
                 body = self.request.get("body")
                 honeypot = self.request.get("required")
             except UnicodeDecodeError:
-                return seld.renderError(400)
+                return self.renderError(400)
 
             if honeypot:
                 # act perfectly normal so the bot thinks the request worked

controllers/post.py
@@ -41,7 +41,7 @@ class PostController(BaseController):
                         body = self.request.get("body", "")
                         honeypot = self.request.get("required")
                     except UnicodeDecodeError:
-                        return seld.renderError(400)
+                        return self.renderError(400)
 
                     if honeypot:
                         # act perfectly normal so the bot thinks the request worked