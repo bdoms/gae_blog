5fef8829b96db4002fda1348a0f5203bb3508c79
bdoms <ravenize@gmail.com>
2014-07-10 08:21:09 -0500

    Fixed verify tokens not actually using salt.

--- controllers/verify.py
+++ controllers/verify.py
@@ -26,9 +26,9 @@ class VerifyController(BaseController):
 def generateToken(url, again=False):
     salt = memcache.get(SALT_KEY)
     if not salt:
-        salt = os.urandom(64)
+        salt = os.urandom(64).encode('base64')
         memcache.set(SALT_KEY, salt)
     now = datetime.utcnow()
     if again:
         now -= timedelta(minutes=1)
-    return sha256(url + now.strftime("%Y%m%d%H%M")).hexdigest()
+    return sha256(url + salt + now.strftime("%Y%m%d%H%M")).hexdigest()
