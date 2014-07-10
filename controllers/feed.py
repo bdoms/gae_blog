6f9dc8982d281a4d36fe278d7f6be7762fb78784
bdoms <ravenize@gmail.com>
2014-07-10 08:22:20 -0500

    Fixed an issue with the feed build time not actually being UTC.

--- controllers/feed.py
+++ controllers/feed.py
@@ -13,5 +13,4 @@ class FeedController(BaseController):
         root_url = self.request.headers.get('host')
 
         # the minifier does not play nice with RSS - CDATA and camelCased end tags are not handled properly
-        self.cacheAndRenderTemplate('feed.rss', minify=False, blog=blog, root_url=root_url, build_date=datetime.now())
-
+        self.cacheAndRenderTemplate('feed.rss', minify=False, blog=blog, root_url=root_url, build_date=datetime.utcnow())
