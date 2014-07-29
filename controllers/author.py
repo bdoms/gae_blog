1def4aae5dbb109d84b59440ad88cbe1fa5f2d6f
bdoms <ravenize@gmail.com>
2014-07-29 01:48:35 -0500

    Improved pagination for SEO.
    Numbered pages now increase with time.
    This way old pages with the same numbers maintain the same content.
    Fixed a bug with author pagination links being wrong.

--- controllers/author.py
+++ controllers/author.py
@@ -1,8 +1,10 @@
-from base import BaseController, renderIfCached
+from base import renderIfCached
+from index import IndexController
 
 from gae_blog import model
 
-class AuthorController(BaseController):
+
+class AuthorController(IndexController):
     """ handles request for an author's page """
 
     @renderIfCached
@@ -13,28 +15,17 @@ class AuthorController(BaseController):
             author = model.BlogAuthor.get_by_id(author_slug, parent=blog.key)
 
             if author:
-                page = 0
-                last_page = 0
-                posts = []
+                author_url = self.blog_url + '/author/' + author_slug
+                result = self.getPaginatedPosts(author, blog.posts_per_page, author_url)
 
-                page_str = self.request.get("page")
-                if page_str:
-                    try:
-                        page = int(page_str)
-                    except:
-                        pass
+                if self.response.status_int != 200:
+                    return
 
-                published_posts = author.published_posts
-                posts_per_page = blog.posts_per_page
-
-                last_page = (published_posts.count() - 1) / posts_per_page
-                if last_page < 0:
-                    last_page = 0
-
-                posts = published_posts.fetch(posts_per_page, offset=page * posts_per_page)
+                page, last_page, posts = result
 
                 page_title = "Author - " + author.name
 
-                return self.cacheAndRenderTemplate('index.html', page=page, last_page=last_page, posts=posts, author=author, page_title=page_title)
+                return self.cacheAndRenderTemplate('index.html', page=page, last_page=last_page, posts=posts,
+                    author=author, author_url=author_url, page_title=page_title)
 
         return self.renderError(404)
--- controllers/index.py
+++ controllers/index.py
@@ -1,3 +1,5 @@
+import math
+
 from base import BaseController, renderIfCached
 
 
@@ -14,20 +16,43 @@ class IndexController(BaseController):
         posts = []
 
         if blog:
-            page_str = self.request.get("page")
-            if page_str:
-                try:
-                    page = int(page_str)
-                except:
-                    pass
+            result = self.getPaginatedPosts(blog, blog.posts_per_page, self.blog_slug)
 
-            published_posts = blog.published_posts
-            posts_per_page = blog.posts_per_page
+            if self.response.status_int != 200:
+                return
 
-            last_page = (published_posts.count() - 1) / posts_per_page
-            if last_page < 0:
-                last_page = 0
-
-            posts = published_posts.fetch(posts_per_page, offset=page * posts_per_page)
+            page, last_page, posts = result
 
         self.cacheAndRenderTemplate('index.html', page=page, last_page=last_page, posts=posts)
+
+
+    def getPaginatedPosts(self, entity, posts_per_page, redirect_url):
+        published_posts = entity.published_posts
+
+        last_page = int(math.ceil(published_posts.count() / float(posts_per_page)))
+        
+        page = 0
+        page_str = self.request.get("page")
+        if page_str:
+            try:
+                page = int(page_str)
+            except:
+                pass
+
+            # don't want this for SEO purposes
+            if not page:
+                return self.redirect(redirect_url, permanent=True)
+            # and these don't exist yet
+            if page >= last_page:
+                return self.renderError(404)
+        else:
+            page = last_page
+        
+        posts = []
+        if page:
+            # invert the offset so that pages increase as time goes on
+            offset_page = last_page - page
+
+            posts = published_posts.fetch(posts_per_page, offset=offset_page * posts_per_page)
+
+        return page, last_page, posts
